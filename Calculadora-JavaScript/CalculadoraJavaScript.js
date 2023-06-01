581757dd99c87585b21d30be65046c357be73441
DillanPD <116019576+DillanPD@users.noreply.github.com>
2023-06-01 09:28:36 -0300

    Add files via upload

---
+++ Calculadora-JavaScript/CalculadoraJavaScript.js
@@ -0,0 +1,35 @@
+function sumarNumeros() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    var resultado = num1 + num2;
+
+    document.getElementById('resultado').innerHTML = "El resultado es: " + resultado;
+}
+
+function restarNumeros() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    var resultado = num1 - num2;
+
+    document.getElementById('resultado').innerHTML = "El resultado es: " + resultado;
+}
+
+function multiplicarNumeros() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    var resultado = num1 * num2;
+
+    document.getElementById('resultado').innerHTML = "El resultado es: " + resultado;
+}
+
+function divivdirNumeros() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    var resultado = num1 / num2;
+
+    document.getElementById('resultado').innerHTML = "El resultado es: " + resultado;
+}
